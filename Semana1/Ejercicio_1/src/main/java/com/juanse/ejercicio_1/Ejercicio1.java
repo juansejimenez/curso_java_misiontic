441b6a73e16ae3d44db2f3666699caad9928141d
Juan Sebastian <juansebas185@hotmail.com>
2021-07-18 17:18:58 -0500

    se agregó nuevo nombre

--- Semana1/Ejercicio_1/src/main/java/com/juanse/ejercicio_1/Ejercicio1.java
+++ Semana1/Ejercicio_1/src/main/java/com/juanse/ejercicio_1/Ejercicio1.java
@@ -5,9 +5,13 @@ public class Ejercicio1 {
     public static void main(String[] args){
         
         var nombre = "Juan Jimenez";
+        var nombre2= "Juan Sebastian 2";
 
         var resultado = saludo(nombre);
-        System.out.println("resultado " + nombre);
+        var resultado2 = saludo(nombre2);
+        
+        System.out.println("resultado " +resultado);
+        System.out.println("resultado " + resultado2);
     }
 
     public static String saludo(String nombre) {
